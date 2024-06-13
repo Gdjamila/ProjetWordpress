753558532857b0aace6e1117d6eeea1f195af698
Gdjamila <djamilagareche@gmail.com>
2024-06-13 11:44:09 +0200

    Mise à jours des pages : accueil et nous rencontrer

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -27,3 +27,31 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+/*  concerne la mise en forme du formulaire contact*/ 
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+}
+/*  pour l'affichage du lien Admine*/
+// Ajout d'une fonction pour filtrer les éléments du menu
+function filter_menu_items($items, $args) {
+    // Vérifie si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+        // Renvoie les éléments du menu tels quels si l'utilisateur est connecté
+        return $items;
+    } else {
+        // Parcourt les éléments du menu
+        foreach ($items as $key => $item) {
+            // Vérifie si le lien est celui de l'admin
+            if ($item->title == 'Admin') {
+                // Supprime l'élément du menu si l'utilisateur n'est pas connecté
+                unset($items[$key]);
+            }
+        }
+        // Renvoie les éléments filtrés du menu
+        return $items;
+    }
+}
+add_filter('wp_nav_menu_objects', 'filter_menu_items', 10, 2);
